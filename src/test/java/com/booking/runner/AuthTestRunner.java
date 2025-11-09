b0c0863a748a60d73c94cf1df13fd9d3c68ac3b3
anbuvelmdev <anbuvelm@hotmail.com>
2025-11-09 12:55:09 +0530

    refactor(runner): add dedicated cucumber runner classes for smoke, regression, and auth tests

--- src/test/java/com/booking/runner/AuthTestRunner.java
+++ src/test/java/com/booking/runner/AuthTestRunner.java
@@ -1,14 +1,15 @@
-package com.booking;
+package com.booking.runner;
 
 import org.junit.platform.suite.api.*;
+
 import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectPackages("com.booking")
-@SelectClasspathResource("features")
+@SelectClasspathResource("features/auth.feature")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.booking")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-reports.html, json:target/cucumber.json")
-public class TestRunner {
+public class AuthTestRunner {
 }
